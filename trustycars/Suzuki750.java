d4e107be65429a282d72434bcb89aaac8715f1af
4LanCrane <125448543+4LanCrane@users.noreply.github.com>
2023-10-04 18:09:41 +0100

    modified:   trustycars/Suzuki750.java

--- trustycars/Suzuki750.java
+++ trustycars/Suzuki750.java
@@ -1,18 +1,15 @@
 package trustycars;
 
-
-public class Suzuki750 extends Bike
-{
-    public Suzuki750(int theYear, int thePrice)
-    {
+public class Suzuki750 extends Bike {
+    public Suzuki750(int theYear, int thePrice) {
         year = theYear;
         price = thePrice;
-        
+
         // fixed values for this particular class
         make = "Suzuki";
         model = "750";
         engineSize = 750;
         fuelCapacity = 20;
-        milesPerLitre = 15;       
-    }    
+        milesPerLitre = 16;
+    }
 }
